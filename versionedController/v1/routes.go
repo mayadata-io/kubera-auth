9845c68d8aab0c9248a5823e5c54f375595be408
Sanjay Nathani <sanjay.nathani@mayadata.io>
2020-11-10 23:07:43 +0530

    Saving GithubAuth Configuration (#18)
    * storing the credentials as in-memory values
    Signed-off-by: Sanjay Nathani <sanjay.nathani@mayadata.io>

--- versionedController/v1/routes.go
+++ versionedController/v1/routes.go
@@ -1,7 +1,8 @@
 package v1
 
 const (
-	TokenRoute    string = "/token"
-	UserRoute     string = "/user"
-	PasswordRoute string = "/password"
+	TokenRoute         string = "/token"
+	UserRoute          string = "/user"
+	PasswordRoute      string = "/password"
+	ConfigurationRoute string = "/configuration"
 )
